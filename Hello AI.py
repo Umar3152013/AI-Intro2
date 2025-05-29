d9f7130bc8207091595ad3f2118085f3ede889ae
Umar <umarmbaig2@gmail.com>
2025-05-29 17:50:34 -0500

    commit changes

--- Hello AI.py
+++ Hello AI.py
@@ -6,3 +6,13 @@ name=input()
 
 #Respond to the user's name
 print(f"Nice to meet you, {name}!")
+# ask a questio
+print(f"how are you doing today ?(good/bad) :")
+mood = input().lower()
+if mood == "good":
+    print(f"I am glad to hear that from you")
+elif mood == "bad":
+    print(f"I am sorry to hear that, hope you feel better")
+else:
+     print(f"I see!")
+print(f"it was nice chatting with you {name}. Good bye")
