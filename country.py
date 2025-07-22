8f1245ed90c559074664aa1cd2671124346339e0
Umar <umarmbaig2@gmail.com>
2025-07-22 11:07:06 -0500

    Add files via upload

---
+++ country.py
@@ -0,0 +1,9 @@
+SPCMcountry_code = {'US':'0001',
+                    'Australia':'0025',
+                    'UK':'0044'}
+print("Country code for US -")
+print(SPCMcountry_code.get('US','Not Found'))
+
+print("Country code for Japan- ")
+print(SPCMcountry_code.get('Japan','Not Found'))
+
