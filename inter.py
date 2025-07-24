304e73aac9f1f2358dd48e57aaf949d99a33fccf
Umar <umarmbaig2@gmail.com>
2025-07-24 12:12:19 -0500

    Add files via upload

---
+++ inter.py
@@ -0,0 +1,8 @@
+seta = {"green", "blue"}
+setb = {"blue", "yellow"}
+print("Original set elements:")
+print(seta)
+print(setb)
+print("\nIntersection of two said sets:")
+setz = seta.intersection(setb)
+print(setz)
